67f69219d60a599987342133d537f24ee7825a33
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2025-08-13 10:30:10 +0200

    Create date-generator.sh

---
+++ draft/date-generator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+year="$1"
+year="2024"
+
+month_shorter=("Jan" "Feb" "March" "Apr" "May" "June" "Jul" "Aug" "Sept" "Oct" "Nov" "Dec")
+month=("January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December")
+len=${#month[*]}
+for (( j=1; j<=len; j++ ));
+do
+    echo "$j-${month[$j-1]}-${year}"
+done
+# date-generator.sh
